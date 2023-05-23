f0279aaaa6b0c981048563540dc3a70781605a38
hneutr <hunter.wapman@gmail.com>
2023-05-23 02:17:12 -0600

    20230523

--- src/htl/notes/init.lua
+++ src/htl/notes/init.lua
@@ -12,7 +12,7 @@ function Notes.sets(path)
     local project = Project.from_path(path)
     local configs = Sets.format_config(project.metadata.notes)
 
-    local sets = {}
+    local sets = Dict()
     for set_key, config in pairs(configs) do
         set_path = Path.joinpath(project.root, set_key)
         sets[set_path] = Sets.get_class(config)(set_path, config)
@@ -23,24 +23,14 @@ end
 
 function Notes.path_set(path)
     local sets = Notes.sets(path)
-    local set_paths = Dict.keys(sets)
-
-    table.sort(set_paths, function(a, b) return #a > #b end)
-
-    for _, set_path in pairs(set_paths) do
-        if set_path == path or Path.is_relative_to(path, set_path) then
-            return sets[set_path]
-        end
-    end
+    sets:filterk(function(k) return k == path or Path.is_relative_to(path, k) end)
+    local set_paths = sets:keys():sort(function(a, b) return #a > #b end)
+    return sets[set_paths[1]]
 end
 
 function Notes.path_sets(path)
-    local sets = Dict()
-    for set_path, set in pairs(Notes.sets(path)) do
-        if set_path == path or Path.is_relative_to(set_path, path) then
-            sets[set_path] = set
-        end
-    end
+    local sets = Notes.sets(path)
+    sets:filterk(function(k) return k == path or Path.is_relative_to(k, path) end)
 
     if #sets:keys() == 0 then
         local set = Notes.path_set(path)
@@ -52,11 +42,11 @@ end
 
 function Notes.path_files(path)
     local files = List()
-    for _, note_set in pairs(Notes.path_sets(path)) do
-        for _, note_path in ipairs(note_set:files(path)) do
-            files:append(note_set:path_file(note_path))
-        end
-    end
+    Notes.path_sets(path):foreachv(function(set)
+        List(set:files(path)):foreach(function(p)
+            files:append(set:path_file(p))
+        end)
+    end)
 
     return files
 end
