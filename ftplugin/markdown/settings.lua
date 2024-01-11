423178375968c8420129c1f0329c047cc44ebca8
hneutr <hunter.wapman@gmail.com>
2024-01-10 23:54:10 -0500

    20240110

--- ftplugin/markdown/settings.lua
+++ ftplugin/markdown/settings.lua
@@ -9,6 +9,13 @@ vim.api.nvim_create_autocmd(
         callback=function()
             vim.bo.autoindent = false
             vim.bo.cindent = false
+            
+            if vim.b.hnetxt_project_root then
+                local Path = require("hl.Path")
+                local project_spellfile = Path(vim.b.hnetxt_project_root):join(".spell", "en.utf-8.add")
+                project_spellfile:parent():mkdir()
+                vim.bo.spellfile = vim.bo.spellfile .. "," .. tostring(project_spellfile)
+            end
         end,
     }
 )
