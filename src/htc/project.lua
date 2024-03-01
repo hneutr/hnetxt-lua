b846525d3fd4977e9a771acb294b3d1ee8b7215c
hneutr <hunter.wapman@gmail.com>
2024-03-01 16:00:27 -0500

    20240301

--- src/htc/project.lua
+++ src/htc/project.lua
@@ -1,5 +1,6 @@
 local Dict = require("hl.Dict")
 local Path = require("hl.Path")
+local List = require("hl.List")
 local Colorize = require("htc.colorize")
 
 local projects = require("htl.db").get().projects
@@ -11,24 +12,31 @@ return {
             projects:get():sorted(function(a, b)
                 return a.created < b.created
             end):foreach(function(p)
-                print(Colorize(p.created, 'yellow') .. ": " .. p.title)
+                local colors = {
+                    bracket = "black",
+                    date = "black",
+                }
+                local s = List({
+                    Colorize("[", colors.bracket),
+                    Colorize(p.created, colors.date),
+                    Colorize("]", colors.bracket),
+                    " ",
+                    p.title,
+                }):join("")
+                print(s)
             end)
         end
     end,
     commands = {
         add = {
-            {"title", default = Path.cwd():name(), args = "1"},
-            {"-p --path", default = Path.cwd(), description = "dir", convert=Path.as_path},
-            {"-d --date", default = os.date("%Y%m%d")},
-            action = function(args)
-                projects:insert({title = args.title, path = args.path, created = args.date})
-            end,
+            {"title", default = Path.cwd():name(), description = "project title", args = "1"},
+            {"-p --path", default = Path.cwd(), description = "project directory", convert=Path.as_path},
+            {"-c --created", default = os.date("%Y%m%d"), description = "project start date"},
+            action = function(args) projects:insert(args) end,
         },
         remove = {
             {"title", default = Path.cwd():name(), args = "1"},
-            action = function(args)
-                projects:remove({title = args.title})
-            end,
+            action = function(args) projects:remove(args) end,
         },
     }
 }
