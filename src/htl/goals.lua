a410e76b73517d56541998b10c510349a532d220
hneutr <hunter.wapman@gmail.com>
2024-06-15 22:31:25 -0400

    20240615

--- src/htl/goals.lua
+++ src/htl/goals.lua
@@ -2,7 +2,7 @@ return function()
     local goals_path = Conf.paths.goals_dir:join(os.date("%Y%m%d") .. ".md")
 
     if not goals_path:exists() then
-        goals_path:write(Conf.paths.daily_intentions_file:read())
+        goals_path:write(Conf.paths.aims_file:read())
     end
 
     return goals_path
