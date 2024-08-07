86aff80f8216001a9c5f87af1fe6ecd5cc1a2108
hneutr <hunter.wapman@gmail.com>
2024-08-07 13:21:01 -0400

    20240807

---
+++ scripts/jobs/daily-commit.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+source /Users/hne/.zshrc
+
+cd /Users/hne/.data
+( gptd ) 2>/dev/null
+
+cd /Users/hne/corpus
+( gptd ) 2>/dev/null
+
+cd /Users/hne/dictionary
+( gptd ) 2>/dev/null
+
+cd /Users/hne/eidola
+( gptd ) 2>/dev/null
